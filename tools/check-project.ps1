e6b1c01e937b697e67b1c06f70dcee956b65f03f
OGISD <buwake58@naver.com>
2026-08-03 22:28:51 +0900

    fix: 조류세기 상대 표기 롤백

--- tools/check-project.ps1
+++ tools/check-project.ps1
@@ -45,6 +45,7 @@ Require-Match $html 'var TIDE_RETRY_MS=30\*60000' '물때 API 자동 재시도 �
 Require-Match $html 'TIDE_BACKUP_TEMPLATE: "data/tide-incheon-\{year\}\.json"' '연간 조석표 백업 경로가 유지됨'
 Require-Match $html 'anchor=tideBackupLast\(prevYmd\)' '첫 물때 증감의 전날 연간표 보충이 유지됨'
 Require-Match $html 'if\(!partial\) try\{ store\.set\(''hb_tidecur''' '조류예측 일부 응답은 하루 캐시에 저장하지 않음'
+Require-NoMatch $html '조류세기\(상대\)' '조류세기 상대지표 표기가 다시 노출되지 않음'
 Require-Match $html 'visibilitychange' '숨긴 페이지의 자동 호출 중지·복귀 처리가 있음'
 Require-NoMatch $html 'allorigins|ENDPOINT_TEMPLATE|useProxy|proxyUrl|fetchRiseSet|RISESET_ENDPOINT' '폐기된 해외 프록시·출몰시각 코드가 없음'
 Require-NoMatch $html '<link[^>]+stylesheet[^>]+https?://' '첫 화면이 외부 글꼴에 의존하지 않음'
